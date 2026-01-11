f6b345bfaf59c6ab3246ef09b1b02483c632c43f
dodyirawan85 <dev.irawans@gmail.com>
2026-01-12 00:11:06 +0700

    sm6375-common: Import dolby stack
    * Guard dolby stack with flags since other device provide the configs but not all
    
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>
    Change-Id: If2b991861dae09c023fc476e8ae94f14e0b8a9a2

--- common.mk
+++ common.mk
@@ -98,6 +98,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
+ifneq ($(TARGET_PROVIDE_DAX),true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+endif
+
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
